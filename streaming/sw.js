fb971bf076ff3f74ad52e5af776db976e723b995
charlystream-bo <jamilpalacios11@gmail.com>
2026-04-18 03:46:42 -0400

    v2.1 auto-servidores Consumet

---
+++ streaming/sw.js
@@ -0,0 +1,5 @@
+﻿const CACHE = 'cs-v2';
+const ASSETS = ['./','./index.html','./styles.css','./app.js','./datos.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { if(e.request.method!=='GET') return; e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))); });
